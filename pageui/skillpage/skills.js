231a481c3038c959380ee1186a299b88609c4417
bht8183 <98561496+bht8183@users.noreply.github.com>
2023-07-04 13:02:47 -0500

    added some information. Added slides to the about page. Added fun transitions to the main page

--- pageui/skillpage/skills.js
+++ pageui/skillpage/skills.js
@@ -1,32 +1,15 @@
-/*
-var button = document.getElementsByClassName("accordion");
-var i;
-for(i=0; i<button.length;i++){
-    button[i].addEventListener("click",function(){
-        this.classList.toggle("active");
-        var desc = this.nextElementSibling;
 
-        if(desc.style.maxHeight){
-            console.log("closing");
-            desc.style.maxHeight = null;
-        }else{
-            console.log("opening");
-            desc.style.maxHeight = desc.scrollHeight + "px" ;
-
-        }
-    });
-} */
 $(document).ready(function(){
     $(".accordion").click(function(){
         this.classList.toggle("active");
         var desc = this.nextElementSibling;
-        
+
         if(desc.style.maxHeight){
             console.log("closing");
             desc.style.maxHeight = null;
         }else{
             console.log("opening");
-            desc.style.maxHeight = desc.scrollHeight + "px" ;
+            desc.style.maxHeight =  "5000px" ;
 
         }
     })
